0481f05ae2d3cb08de026e60edd1f035de75724d
chunkochokun-crypto <chunko.chokun@gmail.com>
2026-09-19 15:19:37 +0700

    Fixed

--- Program1.java
+++ Program1.java
@@ -1,6 +1,6 @@
 class Program1 {
     public static void main(String[] args) {
-        int n = 10;
+        int n = Integer.parseInt(args[0]);
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
